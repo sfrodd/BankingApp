9c8aae794c13b567631c8f1e923279b222b35fd1
sfrodd <sfroddgit@git.edu>
2022-08-04 20:37:15 +0530

    Next Commit

--- src/main/java/com/example/demo/Bank.java
+++ src/main/java/com/example/demo/Bank.java
@@ -4,11 +4,11 @@ public class Bank implements IBank{
 
     public float computeInterest(IBank ib){
 
-        return ib.getBalance()*0.1f;
+        return 0.1f;
     }
     public float computeInterest(RDAccount rd){
 
-        return rd.getRDAmout()*1.5f+200.00f;
+        return 1.5f+200.00f;
     }
 
 }
